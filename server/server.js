53064c9a345a032995c1e3881ede90e1ec20f351
unknown <Asus@example.com>
2018-07-21 22:22:04 +0430

    Add Get /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/todos' , ( req , res ) => {
 	});
 });
 
+app.get('/todos' , (req , res) => {
+	
+	Todo.find().then( (todos) => {
+		res.send({todos});
+	} , (e) => {
+		res.send(e);
+	});
+});
+
 
 app.listen(3000 , () => {
 	console.log('app running on port 3000');
