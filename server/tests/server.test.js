c7c52cff2f0db3689ad5847549829085725da825
unknown <Asus@example.com>
2018-07-24 16:55:02 +0430

    Test the Delete /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -120,6 +120,54 @@ describe('GET /todos/:id' , () => {
 	});
 });	
 
+describe('Delete /todos/:id' , () => {
+	
+	it('should remove a todo' , (done) => {
+		
+		var hexId = todos[0]._id.toHexString();
+		
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect( (res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end( ( err , res ) => {
+				
+				if(err){
+					return done(err);
+				}
+				
+				Todo.findById(hexId).then( (todo) => {
+					expect(todo).toBeTruthy()();
+					done();
+				}).catch( (e) => {
+					done(e);
+				});
+			});		
+	});
+	
+	it('should return 404 if todo not found' , (done) => {
+		
+		var hexId = new ObjectID().toHexString();
+		
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+	
+	it('should return 404 if objectId is invalid' , (done) => {
+		
+		var hexId = '123abc';
+		
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+});
+
 
 
 
