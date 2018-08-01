b070a4ab617d287aca06372b29f94cc8e039bec9
unknown <Asus@example.com>
2018-08-01 12:40:45 +0430

    Add POST /user/login route

--- server/models/User.js
+++ server/models/User.js
@@ -86,6 +86,33 @@ UserSchema.pre('save' , function (next){
 	}
 });
 
+UserSchema.statics.findByCredentials = function (email , password){
+	var User = this ;
+	
+	return User.findOne({email}).then( (user) => {
+		if(!user){
+			return Promisr.reject();
+		}
+		
+		return new Promise( (resolve , reject) => {
+			
+			bcrypt.compare(password , user.password , (err , res) => {
+				if(err){
+					reject();
+				}
+				if(res === true){
+					resolve(user);
+				}
+				else{
+					reject()
+				}
+			});
+			
+		});
+	});
+	
+}
+
 var User = mongoose.model( 'User',UserSchema);
 
 module.exports = { User };
